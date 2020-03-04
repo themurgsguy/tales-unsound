ad938065fbb0fdec4ef965c27e78de55af8e15e8
themurgsguy <ernest.da.cruz@gmail.com>
2020-03-04 01:19:57 +0000

    Ajax are now semi working

--- outback/views.py
+++ outback/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import get_object_or_404, render
+from django.forms import model_to_dict
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Model
 from django.http import JsonResponse
 from django.views.generic import View
 
 from .models import Place
+
+
+class ExtendedEncoder(DjangoJSONEncoder):
+    def default(self, o):
+        if isinstance(o, Model):
+            return model_to_dict(o)
+        return Super().default(o)
+
 
 class Outback(View):
     http_method_name = ['get', 'post']
@@ -12,15 +24,17 @@ class Outback(View):
         return render(request, 'outback/index.html', {'place': current_place})
 
     def post(self, request):
-        current_place = request.POST['from']
-        requested_place = request.POST['to']
-        print('current_space: {}'.format(current_place))
-        print('requested_space: {}'.format(requested_place))
-
         try:
-            place = get_object_or_404(Place, name=requested_place)
-            return JsonResponse({'name': place.name, 'description': place.description })
+            current_place = get_object_or_404(Place, name=request.POST['from'])
+            requested_place = current_place.exits.get(name=request.POST['to'])
+
+            response = {
+                'name': requested_place.name,
+                'description': requested_place.description,
+                'exits': [e.description for e in requested_place.exit_set.all()]
+            }
+            return JsonResponse(response)
         except:
-            return JsonResponse({'name': current_place, 'description': 'That space does not existe.'})
+            return JsonResponse({'name': current_place.name, 'description': 'That space does not existe.'})
 
 outback = Outback.as_view()
