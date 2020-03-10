36fc80833bc044a1208a47cd3bf1cad23f89e5ba
themurgsguy <ernest.da.cruz@gmail.com>
2020-03-10 01:16:17 +0000

    Started fleshing out new frontend app in vue

---
+++ outback_vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <terminal></terminal>
+  </div>
+</template>
+
+<script>
+import Terminal from '@/components/Terminal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Terminal
+  }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/common.scss";
+
+#app {
+  font-family: $font-stack;
+  font-size: 1.5em; // 24px
+  line-height: 1.5;
+  height: 100vh;
+}
+</style>
---
+++ outback_vue/src/components/Terminal.vue
@@ -0,0 +1,61 @@
+<template>
+  <b-container class="terminal">
+    <b-row align-h="center">
+      <b-col cols="9">
+        <p class="red">{{ placeName }}</p>
+      </b-col>
+    </b-row>
+
+    <b-row align-h="center" class="mt-auto">
+      <b-col cols="9">
+        <p v-for="(log, index) in backlog" :key="index">{{ log }}</p>
+      </b-col>
+    </b-row>
+
+    <b-row align-h="center" class="mt-3">
+      <b-col cols="9">
+        <b-form-group>
+          <b-form-input type="text" v-model="prompt" @keyup.enter="onEnter" autofocus></b-form-input>
+        </b-form-group>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'terminal',
+  data: () => ({
+    backlog: [],
+    prompt: ''
+  }),
+  computed: {
+    placeName () {
+      return 'Shlong'
+    }
+  },
+  methods: {
+    onEnter () {
+      this.backlog.push(this.prompt)
+      this.prompt = ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .terminal {
+    display: flex;
+    flex-direction: column;
+    padding-top: 1em;
+    padding-bottom: 1em;
+    height: 100%;
+    border: solid 1px red;
+  }
+
+  .terminal .form-control {
+    height: calc(1.5em + 0.75em + 2px);
+    padding: 0.375em 0;
+    font-size: 1em;
+  }
+</style>
